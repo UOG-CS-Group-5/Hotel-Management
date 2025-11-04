eda37ab5a45691abb836dcb2e5692b5e49f27fce
BuildTools <unconfigured@null.spigotmc.org>
2025-11-04 20:55:19 +1000

    Added Main, Customer, Reservation, CustomerManager, ReservationManager, ConsoleUI, DatabaseConnection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.hotelmanagement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.hotelmanagement to javafx.fxml;
     exports com.example.hotelmanagement;
+    exports com.example.hotelmanagement.database;
+    opens com.example.hotelmanagement.database to javafx.fxml;
 }
