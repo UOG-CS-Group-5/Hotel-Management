24c361b01b21fe6582841fe07734d8cb174175fd
BuildTools <unconfigured@null.spigotmc.org>
2025-11-04 22:51:50 +1000

    Added database to project and instructions to replace database connection URL on local computer

--- src/main/java/com/example/hotelmanagement/database/DatabaseConnection.java
+++ src/main/java/com/example/hotelmanagement/database/DatabaseConnection.java
@@ -8,7 +8,11 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     // path to database file
     private static final String DATABASEURL =
-            "jdbc:ucanaccess://C://Users//James//Documents//.School Stuff//Work//HotelManagementSoftwareDatabase.accdb";
+            "jdbc:ucanaccess://C:\\Users\\James\\IdeaProjects\\Hotel-Management\\src\\main\\java\\com\\example\\hotelmanagement\\database\\HotelManagementSoftwareDatabase.accdb";
+    // *Using Intellij
+    // To get database file, right click DB file in the project explorer on the left
+    // Select "Copy Path/Reference" and select "Absolute Path"
+    // Replace after "jdbc:ucanaccess://" in the URL above
 
     // Returns a connection from the database
     public static Connection getConnection() throws SQLException {
