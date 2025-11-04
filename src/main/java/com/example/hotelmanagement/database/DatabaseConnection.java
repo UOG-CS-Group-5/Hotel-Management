74ef81d48dff3a39ab824e432283a994afe81dea
BuildTools <unconfigured@null.spigotmc.org>
2025-11-04 21:34:03 +1000

    Updated comments

--- src/main/java/com/example/hotelmanagement/database/DatabaseConnection.java
+++ src/main/java/com/example/hotelmanagement/database/DatabaseConnection.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 
 // Creates connection to Microsoft Access database
 public class DatabaseConnection {
+    // path to database file
     private static final String DATABASEURL =
             "jdbc:ucanaccess://C://Users//James//Documents//.School Stuff//Work//HotelManagementSoftwareDatabase.accdb";
 
+    // Returns a connection from the database
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASEURL);
     }
